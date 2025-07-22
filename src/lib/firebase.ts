ee4032c899e9644ebcda6148472a4cbe167b1fc3
Soroi Collection <soroicollections@gmail.com>
2025-07-22 13:45:19 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  projectId: "assetwise-jaw4u",
+  appId: "1:666491814950:web:c3f262bbcda6d7f93692e7",
+  storageBucket: "assetwise-jaw4u.firebasestorage.app",
+  apiKey: "AIzaSyDQcP-DTmb4yoXtj5Hq4cTaQvaUkZFva2A",
+  authDomain: "assetwise-jaw4u.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "666491814950",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth, app };
