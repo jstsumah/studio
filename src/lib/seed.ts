cdd144cff0b19c6cc5832c3136915f821bd9b843
Jackson Tsumah <JStsumah@gmail.com>
2025-07-28 19:09:15 +0000

    go ahead and update package as advised

--- src/lib/seed.ts
+++
@@ -1,5 +0,0 @@
-// This file is intended for development purposes only and is not included in the production build.
-// You can use this file to seed your database with initial data.
-// To run this script, use the command: `npm run seed`
-
-console.log('Seed script is for development use. Not needed for production.');
