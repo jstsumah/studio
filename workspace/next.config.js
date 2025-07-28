2286da9d21eda4e9e41745896c66f6b34d86614c
Jackson Tsumah <JStsumah@gmail.com>
2025-07-28 19:28:52 +0000

    starting build "bf8274e2-75ad-402c-be39-9d0166ef34a6" FETCHSOURCE BUILD

---
+++ workspace/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com'
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
